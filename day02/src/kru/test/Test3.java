b8d60d7bb708bdb6af11b3a02956efb0693ebc2d
modemneko <xavier.i@aliyun.com>
2026-02-06 16:57:26 +0800

    Update Test3.java code

--- day02/src/kru/test/Test3.java
+++ day02/src/kru/test/Test3.java
@@ -14,12 +14,8 @@ public class Test3 {
         System.out.println("请输入第二个数：");
         int b = input.nextInt();
 
-        if (a == 6 || b == 6) {
-            if ((a + b) % 6 == 0) {
-                System.out.println(true);
-            } else  {
-                System.out.println(false);
-            }
+        if ((a == 6 || b == 6) || ((a + b) % 6 == 0)) {
+            System.out.println(true);
         } else {
             System.out.println(false);
         }
