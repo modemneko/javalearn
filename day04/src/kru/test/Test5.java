ba062d0b66467eb92e8e0426268ea189d9f70325
modemneko <xavier.i@aliyun.com>
2026-02-10 00:09:29 +0800

    Update

--- day04/src/kru/test/Test5.java
+++ day04/src/kru/test/Test5.java
@@ -1,5 +1,7 @@
 package kru.test;
 
+import java.util.Scanner;
+
 public class Test5 {
     public static void main(String[] args) {
 //        在实际开发中，电影院选座也会使用到if判断。
@@ -9,5 +11,20 @@ public class Test5 {
 //        根据不同情况，给出不同的提示：
 //        如果票号为奇数，那么打印坐左边
 //        如果票号为偶数，那么打印坐右边。
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("请输入你的票号：");
+        int n = sc.nextInt();
+
+        if (n >= 1 && n <= 100) {
+            if (n % 2 != 0) {
+                System.out.println("坐左边");
+            }  else {
+                System.out.println("坐右边");
+            }
+        } else {
+            System.out.println("输入超过范围！");
+        }
+
     }
 }
