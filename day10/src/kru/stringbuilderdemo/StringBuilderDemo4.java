fe8e6ef845c241b54d5332447f51bfaf4bc82d23
modemneko <xavier.i@aliyun.com>
2026-04-16 23:43:15 +0800

    Update

--- day10/src/kru/stringbuilderdemo/StringBuilderDemo4.java
+++ day10/src/kru/stringbuilderdemo/StringBuilderDemo4.java
@@ -1,19 +1,16 @@
 package kru.stringbuilderdemo;
 
+import java.util.Scanner;
+
 public class StringBuilderDemo4 {
     public static void main(String[] args) {
         // 创建对象
         StringBuilder sb = new StringBuilder();
 
-        sb.append("aaa");
-        sb.append("bbb");
-        sb.append("ccc");
-        sb.append("ddd");
+        String str = sb.append("aaa").append("bbb").append("ccc").append("ddd").toString();
 
-        System.out.println(sb);
+        // System.out.println(sb);
 
-        // 再把StringBuilder变回字符串
-        String str = sb.toString();
         System.out.println(str);
     }
 }
