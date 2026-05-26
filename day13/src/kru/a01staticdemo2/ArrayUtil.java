b85842a1a9c620a64827bbb91b84fda383a5c347
modemneko <xavier.i@aliyun.com>
2026-05-26 20:06:35 +0800

    Update

--- day13/src/kru/a01staticdemo2/ArrayUtil.java
+++ day13/src/kru/a01staticdemo2/ArrayUtil.java
@@ -10,15 +10,16 @@ public class ArrayUtil {
     private ArrayUtil() {}
 
     public static String printArr(int[] arr) {
-        String result = "[";
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
         for (int i = 0; i < arr.length; i++) {
-            result += arr[i];
+            sb.append(arr[i]);
             if (i < arr.length - 1) {
-                result += ",";
+                sb.append(", ");
             }
         }
-        result += "]";
-        return result;
+        sb.append("]");
+        return sb.toString();
     }
 
     public static double getAerage(double[] arr) {
