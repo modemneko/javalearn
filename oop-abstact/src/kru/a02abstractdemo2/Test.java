0a9c75e20844c22c60b80a80abd5da7e6f819484
modemneko <xavier.i@aliyun.com>
2026-06-15 22:14:00 +0800

    Update
    Signed-off-by: modemneko <xavier.i@aliyun.com>

--- oop-abstact/src/kru/a02abstractdemo2/Test.java
+++ oop-abstact/src/kru/a02abstractdemo2/Test.java
@@ -4,16 +4,19 @@ public class Test {
     public static void main(String[] args) {
         Frog frog = new Frog("wawa",20);
 
+        frog.show();
         frog.drink();
         frog.eat();
 
         Dog dog = new Dog("doge", 24);
 
+        dog.show();
         dog.drink();
         dog.eat();
 
         Sheep sheep = new Sheep("sheet", 19);
 
+        sheep.show();
         sheep.drink();
         sheep.eat();
 
